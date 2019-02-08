3d571ddda54c2457faa483a9e6e64b6a7d0d3656
JurajB <JurajB@kentico.com>
2019-02-08 09:47:42 +0100

    KA-468 prepare services for end to end tests (#8)
    Update links in package.json
    Inject "isTest" attribute and pass it to event Grid

--- kcd-webhook-service/publishEventsCreator.js
+++ kcd-webhook-service/publishEventsCreator.js
@@ -4,4 +4,4 @@ module.exports = dependencies =>
     async (events) => {
         const docsChangedHost = url.parse(dependencies.host, true).host;
         return dependencies.eventGridClient.publishEvents(docsChangedHost, events);
-    }
+    };
--- kcd-webhook-service/publishEventsCreator.test.js
+++ kcd-webhook-service/publishEventsCreator.test.js
@@ -2,10 +2,10 @@ const publishEventsCreator = require('./publishEventsCreator');
 
 const eventGridClient = {
     publishEvents: jest.fn()
-}
+};
 const host = 'fake.url-to-webhook.cloud';
 const fakeHost = `http://${host}/api/webhook`;
-const events =[{
+const events = [{
     subject: 'test',
     eventType: 'test_event',
     dataVersion: '1.0',
@@ -14,15 +14,15 @@ const events =[{
 }];
 
 describe('publishEvents', () => {
-  test('calls publishEvents with correct host and events', async () => {
-    const deps = {
-        eventGridClient,
-        host: fakeHost,
-    };
+    test('calls publishEvents with correct host and events', async () => {
+        const deps = {
+            eventGridClient,
+            host: fakeHost,
+        };
 
-    await publishEventsCreator(deps)(events);
-    
-    expect(eventGridClient.publishEvents.mock.calls[0][0]).toBe(host);
-    expect(eventGridClient.publishEvents.mock.calls[0][1]).toBe(events);
-  });
+        await publishEventsCreator(deps)(events);
+
+        expect(eventGridClient.publishEvents.mock.calls[0][0]).toBe(host);
+        expect(eventGridClient.publishEvents.mock.calls[0][1]).toBe(events);
+    });
 });
