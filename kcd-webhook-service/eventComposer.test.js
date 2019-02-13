1b7fe5b8583dcfbb98c51ee6f2afedcfbfe82bdb
Juraj Bielik <jurajb@kentico.com>
2019-02-13 14:27:04 +0100

    KA-468 Update unit tests

--- kcd-webhook-service/eventComposer.test.js
+++ kcd-webhook-service/eventComposer.test.js
@@ -13,7 +13,7 @@ const webhookBody = {
 describe('eventComposer', () => {
     test('composes event with data from webhook', async () => {
         const eventType = 'kentico';
-        const isTest = null;
+        const isTest = undefined;
         const event = eventComposer(webhookBody, eventType, isTest);
 
         expect(event.id).toBeTruthy();
@@ -27,7 +27,7 @@ describe('eventComposer', () => {
 
     test('composes event with invalid type', async () => {
         const eventType = 'notKentico';
-        const isTest = null;
+        const isTest = undefined;
         const event = eventComposer({}, eventType, isTest);
 
         expect(event.id).toBeTruthy();
@@ -41,11 +41,25 @@ describe('eventComposer', () => {
 
     test('composes event with testing configuration', async () => {
         const eventType = 'kentico';
-        const isTest = true;
+        const isTest = 'true';
         const event = eventComposer(webhookBody, eventType, isTest);
 
         expect(event.id).toBeTruthy();
         expect(event.isTest).toBe(true);
+        expect(event.subject).toBe(webhookBody.message.operation);
+        expect(event.eventType).toBe(eventType);
+        expect(event.dataVersion).toBe('1.0');
+        expect(event.data).toBe(webhookBody.data);
+        expect(event.eventTime).toBeTruthy();
+    });
+
+    test('composes event with incorrect testing configuration', async () => {
+        const eventType = 'kentico';
+        const isTest = 'something';
+        const event = eventComposer(webhookBody, eventType, isTest);
+
+        expect(event.id).toBeTruthy();
+        expect(event.isTest).toBe(false);
         expect(event.subject).toBe(webhookBody.message.operation);
         expect(event.eventType).toBe(eventType);
         expect(event.dataVersion).toBe('1.0');
