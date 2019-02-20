a9c13dfa56468748d8c4f541ab92d99486fce018
jirik <jirik@kentico.com>
2019-02-20 10:00:22 +0100

    KA-506 Fix integration test support

--- kcd-webhook-service/eventComposer.test.js
+++ kcd-webhook-service/eventComposer.test.js
@@ -17,25 +17,25 @@ describe('eventComposer', () => {
         const event = eventComposer(webhookBody, eventType, isTest);
 
         expect(event.id).toBeTruthy();
-        expect(event.test).toBe(undefined);
         expect(event.subject).toBe(webhookBody.message.operation);
         expect(event.eventType).toBe(eventType);
         expect(event.dataVersion).toBe('1.0');
-        expect(event.data).toBe(webhookBody.data);
+        expect(event.data.webhook).toBe(webhookBody.data);
+        expect(event.data.test).toBe('disabled');
         expect(event.eventTime).toBeTruthy();
     });
 
     test('composes event with testing configuration', async () => {
         const eventType = 'kentico';
-        const isTest = 'true';
+        const isTest = 'enabled';
         const event = eventComposer(webhookBody, eventType, isTest);
 
         expect(event.id).toBeTruthy();
-        expect(event.test).toBe('true');
         expect(event.subject).toBe(webhookBody.message.operation);
         expect(event.eventType).toBe(eventType);
         expect(event.dataVersion).toBe('1.0');
-        expect(event.data).toBe(webhookBody.data);
+        expect(event.data.webhook).toBe(webhookBody.data);
+        expect(event.data.test).toBe('enabled');
         expect(event.eventTime).toBeTruthy();
     });
 
@@ -45,11 +45,10 @@ describe('eventComposer', () => {
         const event = eventComposer(webhookBody, eventType, isTest);
 
         expect(event.id).toBeTruthy();
-        expect(event.test).toBe('something');
         expect(event.subject).toBe(webhookBody.message.operation);
         expect(event.eventType).toBe(eventType);
-        expect(event.dataVersion).toBe('1.0');
-        expect(event.data).toBe(webhookBody.data);
+        expect(event.data.webhook).toBe(webhookBody.data);
+        expect(event.data.test).toBe('disabled');
         expect(event.eventTime).toBeTruthy();
     });
 });
